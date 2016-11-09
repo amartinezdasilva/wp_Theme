8a32a013434dce845bbdfdc4d052700890f140b1
Aaron <amartinezdasilva@danielcastelao.org>
2016-11-09 09:40:57 +0000

    el bicho

---
+++ header.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <p>Hola mundo mundial</p>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<?php get_header(); ?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <p>Hola mundoo</p>
+        <?php get_footer();?>
+    </body>
+</html>
